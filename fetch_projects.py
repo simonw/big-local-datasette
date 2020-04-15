0584e0fe3f3c04a9c89e9a6f5de041d1e8d52d0d
Simon Willison <swillison@gmail.com>
2020-04-15 12:28:15 -0700

    Only fetch biglocalnews@ owned projects
    Refs #14.
    
    Also disabled GitHub Actions database file caching, partly to ensure
    the previous database is deleted before running this, and partly
    because it doesn't work with scheduled anyway - refs #18

--- fetch_projects.py
+++ fetch_projects.py
@@ -1,5 +1,5 @@
 # Fetch all project information into a SQLite database
-import sqlite_utils, requests, sys
+import sqlite_utils, requests, sys, click
 
 graphql_query = """
 {
@@ -27,17 +27,26 @@ graphql_query = """
 """
 
 
-def fetch_projects(db, token):
+@click.command()
+@click.argument("db_path", type=click.Path(file_okay=True, dir_okay=False))
+@click.argument("big_local_token")
+@click.option(
+    "--contact", multiple=True, help="Only fetch projects owned by these emails"
+)
+def fetch_projects(db_path, big_local_token, contact):
+    db = sqlite_utils.Database(db_path)
     response = requests.post(
         "https://api.biglocalnews.org/graphql",
         json={"query": graphql_query},
-        headers={"Authorization": "JWT {}".format(token)},
+        headers={"Authorization": "JWT {}".format(big_local_token)},
     )
     assert 200 == response.status_code, response.status_code
     data = response.json()
     for edge in data["data"]["openProjects"]["edges"]:
         project = edge["node"]
         files = project.pop("files")
+        if contact and project["contact"] not in contact:
+            continue
         db["projects"].insert(project, pk="id", replace=True)
         if files:
             db["files"].upsert_all(
@@ -55,6 +64,4 @@ def fetch_projects(db, token):
 
 
 if __name__ == "__main__":
-    token = sys.argv[-1]
-    db = sqlite_utils.Database("biglocal.db")
-    fetch_projects(db, token)
+    fetch_projects()
